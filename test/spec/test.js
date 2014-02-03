52ab32f54f46418f543334980e32da601f78cbf2
heartyoh <heartyoh@gmail.com>
2014-02-04 00:55:07 +0900

    test code

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,24 @@
 (function () {
     'use strict';
 
+    describe('모델러의 생성에 있어서', function() {
+    	describe('Dimension과 관련해서는', function() {
+    		var modeler = new Delo.DocumentView({
+				el : undefined,
+			    collection : new Delo.Document()
+        	});
+
+    		it('기본 폭은 800 픽셀이다.', function() {
+            	modeler.collection.width.should.equal(800);
+    		});
+    		it('기본 높이은 600 픽셀이다.', function() {
+            	modeler.collection.height.should.equal(600);
+    		});
+    	})
+    })
+
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-            	Delo.DocumentView.should.equal(undefined);
-            });
             it('should run here few assertions 2', function () {
             	[1,2,3].indexOf(4).should.equal(-1);
             });
